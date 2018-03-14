c13473a0b0679b7fd07207bd0e3d961a01fe4c58
Andrii Kulikov <jadecoresky@gmail.com>
2018-03-15 01:04:16 +0200

    Initial
    Initial commit

--- EverGreenBot/Program.cs
+++ EverGreenBot/Program.cs
@@ -1,12 +1,56 @@
-﻿using System;
+﻿using Discord;
+using Discord.WebSocket;
 
-namespace EverGreenBot
+using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+
+namespace EverGreen
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private DiscordSocketClient _client;
+        public static IConfiguration Configuration { get; set; }
+
+        public static void Main(string[] args) {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+
+            new Program().MainAsync().GetAwaiter().GetResult();
+        }
+
+        public async Task MainAsync()
         {
-            Console.WriteLine("Hello World!");
+            var _config = new DiscordSocketConfig { MessageCacheSize = 100 };
+
+            _client = new DiscordSocketClient(_config);
+
+            _client.Log += Log;
+            _client.MessageReceived += MessageReceived;
+
+            await _client.LoginAsync(TokenType.Bot, Configuration["AppConfiguration:token"]);
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceived(SocketMessage message)
+        {
+            if (message.Content == "!ping")
+            {
+                await message.Channel.SendMessageAsync("Pong!");
+            }
         }
     }
 }
